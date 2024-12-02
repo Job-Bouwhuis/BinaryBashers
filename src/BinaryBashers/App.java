4e99b0613feb93a69b1b642bd0b9a049014168ea
Job Bouwhuis <jobbouwhuis@hotmail.com>
2024-12-02 12:49:28 +0100

    fixed App after merge

--- src/BinaryBashers/App.java
+++ src/BinaryBashers/App.java
@@ -1,10 +1,8 @@
 package BinaryBashers;
 
+import BinaryBashers.UI.DialogBoxes.DialogBoxManager;
 import dev.WinterRose.SaxionEngine.*;
-import dev.WinterRose.SaxionEngine.Button;
 import nl.saxion.app.SaxionApp;
-
-import java.awt.*;
 
 public class App extends Application
 {
@@ -19,21 +17,10 @@ public class App extends Application
     public void createScenes()
     {
         createScene("testScene", scene -> {
-            Sprite sprite = Sprite.square(10, 10, Color.white);
-
-            GameObject text = new GameObject("textstuff");
-            var inrend = new TextRenderer("This is some awesome text \ndid you know that? \nNo? \nWell you should");
-//            inrend.getTextProvider().getWords().get(2).fontType = FontType.Bold;
-//            inrend.getTextProvider().getWords().get(6).setColor(Color.red);
-//            inrend.getTextProvider().getWords().get(8).getCharacters()[2].color = Color.magenta;
-            text.addComponent(inrend);
-            scene.addObject(text);
-
             GameObject button = new GameObject("button");
             button.transform.setPosition(new Vector2(200, 100));
-            var butt = new Button(Sprite.square(30, 20, Color.white));
-            button.addComponent(butt);
-            butt.onClick.add(b -> closeGame());
+            var boxManager = new DialogBoxManager();
+            button.addComponent(boxManager);
 
             scene.addObject(button);
         });
@@ -43,20 +30,21 @@ public class App extends Application
             //backgroundSprite = SpritePalletChanger.changePallet(backgroundSprite, new ColorPallet(new Sprite("resources/colorPallets/main.png"), new Sprite("resources/colorPallets/midnightAblaze/midnight-ablaze.png")));
 
             var spriteRenderer = new SpriteRenderer(backgroundSprite);
-            spriteRenderer.origin = new Vector2(0,0);
+            spriteRenderer.origin = new Vector2(0, 0);
             GameObject backgroundObject = new GameObject("background");
             backgroundObject.addComponent(spriteRenderer);
             scene.addObject(backgroundObject);
 
             EnemySprite enemySprite = new EnemySprite();
             enemySprite.setSpriteId(SaxionApp.getRandomValueBetween(0, 2));
-//            enemySprite.startEnteringAnimation();
+            //            enemySprite.startEnteringAnimation();
             GameObject enemyObject = new GameObject("TestEnemySprite");
             enemyObject.addComponent(enemySprite);
             enemyObject.transform.setPosition(new Vector2(200, 200));
             scene.addObject(enemyObject);
+        });
 
-
+        loadScene("testScene");
     }
 
     @Override
