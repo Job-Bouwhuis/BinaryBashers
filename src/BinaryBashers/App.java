1a4aec032739c30e3551018d29aa5e82d757d8ff
AlmarvDiessen <0330841@student.rocvantwente.nl>
2024-12-11 14:06:14 +0100

    added Binary level, fixed Binary enemy comparision

--- src/BinaryBashers/App.java
+++ src/BinaryBashers/App.java
@@ -2,10 +2,12 @@ package BinaryBashers;
 
 import BinaryBashers.Enemies.EnemySprite;
 import BinaryBashers.UI.DialogBoxes.DialogBoxManager;
+import BinaryBashers.Utils.Util;
 import dev.WinterRose.SaxionEngine.*;
 import BinaryBashers.Enemies.BinaryEnemy;
 import dev.WinterRose.SaxionEngine.ColorPallets.ColorPallet;
 import BinaryBashers.Enemies.EnemySpawner;
+import dev.WinterRose.SaxionEngine.Levels.BinaryToDecimal;
 import nl.saxion.app.SaxionApp;
 
 public class App extends Application
@@ -64,47 +66,9 @@ public class App extends Application
             scene.addObject(timerObject);
             scene.setScenePallet(new ColorPallet(new Sprite("resources/colorPallets/midnightAblaze/midnight-ablaze.png")));
         });
+         new BinaryToDecimal().createScene(this);
 
-
-        createScene("spawnerTest", scene -> {
-            Sprite backgroundSprite = new Sprite("resources/sprites/background/background01.png");
-
-            var spriteRenderer = new SpriteRenderer(backgroundSprite);
-            spriteRenderer.origin = new Vector2(0, 0);
-            GameObject backgroundObject = new GameObject("background");
-            backgroundObject.addComponent(spriteRenderer);
-            scene.addObject(backgroundObject);
-            EnemySpawner<?> enemySpawner = new EnemySpawner<>(BinaryEnemy.class);
-            GameObject spawner = new GameObject("spawner");
-            Sprite timerSprite = new Sprite("resources/sprites/ui/timer/Timer1.png");
-            Timer enemySpawnTimer = new Timer(5, true, true, 1);
-            Timer playerDamageTimer = new Timer(10, true, true, 1);
-            Player player = new Player(3, enemySpawner);
-            GameObject playerObj = new GameObject("player");
-            playerObj.addComponent(player);
-            playerObj.addComponent(playerDamageTimer);
-
-
-            player.transform.setPosition(new Vector2(Painter.renderWidth - timerSprite.getwidth(), Painter.renderHeight - timerSprite.getHeight()));
-            spawner.addComponent(enemySpawner);
-            spawner.addComponent(enemySpawnTimer);
-            scene.addObject(spawner);
-            scene.addObject(playerObj);
-            GameObject inputField = new GameObject("inputRenderer");
-            InputRenderer inputRenderer = new InputRenderer(4);
-            enemySpawner.inputRenderer = inputRenderer;
-            inputRenderer.onEnterKeyPressed.add(inputRenderer1 -> {
-                enemySpawner.checkAndKillEnemies(inputRenderer1.getInputAsString());
-                inputRenderer1.inputText.clear();
-            });
-            inputRenderer.acceptedCharacters = new Character[]{'0', '1'};
-            inputField.addComponent(inputRenderer);
-            inputField.transform.setPosition(new Vector2(Painter.renderCenter).add(new Vector2(0, (float) Painter.renderHeight / 2)));
-            scene.addObject(inputField);
-            scene.setScenePallet(new ColorPallet(new Sprite("resources/colorPallets/midnightAblaze/midnight-ablaze.png")));
-        });
-
-        loadScene("spawnerTest");
+        loadScene("BinaryLevel");
     }
 
     @Override
